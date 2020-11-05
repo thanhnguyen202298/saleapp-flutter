1f076f5dfbed127bff35d4be34e056de41a1f126
thanhnguyen202298 <thanh@fuvi.vn>
2020-11-05 18:04:43 +0700

    custome view

--- lib/main.dart
+++ lib/main.dart
@@ -34,6 +34,7 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+
   void _incrementCounter() {
     setState(() {
       // _counter++;
@@ -46,21 +47,8 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      drawer: MyDraw(context),
+      body: BodyPage(_counter),
+      drawer: MyDrawer(context),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
@@ -71,10 +59,33 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 }
 
-class MyDraw extends StatelessWidget{
-  MyDraw(this.context):super();
+class BodyPage extends StatelessWidget {
+  BodyPage(this._counter) : super();
+  final int _counter;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text(
+            'You have pushed the button this many times:',
+          ),
+          Text(
+            '$_counter',
+            style: Theme.of(context).textTheme.headline4,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class MyDrawer extends StatelessWidget {
+  MyDrawer(this.context) : super();
   final BuildContext context;
-  final double radial = 50;
+  final double radial = 80;
   final snackBar = SnackBar(
     content: Text('Yay! A SnackBar!'),
     action: SnackBarAction(
@@ -91,30 +102,20 @@ class MyDraw extends StatelessWidget{
       child: ListView(
         children: [
           DrawerHeader(
-            margin: EdgeInsets.all(2),
-            decoration: ShapeDecoration.fromBoxDecoration(BoxDecoration(
-                shape: BoxShape.circle,
-                color: Colors.green,
-                border: Border(),
-                image: DecorationImage(
-                  image: Image.network(
-                      "https://9mobi.vn/cf/images/2015/03/nkk/anh-dep-1.jpg")
-                      .image,
-                ))),
-            child: Center(
-                child: CircleAvatar(
-                  radius: radial,
-                  backgroundColor: Colors.amber,
-                  backgroundImage: NetworkImage(
-                      "https://9mobi.vn/cf/images/2015/03/nkk/anh-dep-1.jpg"),
-                )),
+            // decoration: CircleDecor("https://9mobi.vn/cf/images/2015/03/nkk/anh-dep-1.jpg").build(),
+            padding: EdgeInsets.fromLTRB(20, 0, 20, 0),
+            margin: EdgeInsets.fromLTRB(0, 0, 0, 0),
+            child: CircleImage("", radial: 80,),
           ),
           ListTile(
             title: Text("Bills"),
             onTap: () => {Navigator.pop(context)},
           ),
           ListTile(
-            title: CaptionText("Hover text example"),
+            title: CaptionText(
+              "Hover text example",
+              fontsize: 30,
+            ),
             onTap: () {
               Scaffold.of(this.context).showSnackBar(snackBar);
             },
@@ -126,11 +127,66 @@ class MyDraw extends StatelessWidget{
 }
 
 class CaptionText extends StatelessWidget {
-  CaptionText(this.data) : super();
-  String data = "TexttureTor";
+  CaptionText(this.data,
+      {this.fontsize,
+      this.color,
+      this.bold,
+      this.paddingBox = 0,
+      this.alignText = TextAlign.center})
+      : super();
+  final String data;
+  final double fontsize;
+  final double paddingBox;
+  final Color color;
+  final TextAlign alignText;
+  final FontWeight bold;
 
   @override
   Widget build(BuildContext context) {
-    return Text(data, style: TextStyle(fontSize: 20));
+    return Padding(
+      padding: EdgeInsets.all(this.paddingBox),
+      child: Text(
+        data,
+        style: TextStyle(
+            fontSize: this.fontsize, color: this.color, fontWeight: this.bold),
+        textAlign: this.alignText,
+      ),
+    );
+  }
+}
+class CircleDecor {
+  CircleDecor(this.url, {this.color}) : super();
+  final String url;
+  final Color color;
+
+  @override
+  Decoration build() {
+    return ShapeDecoration.fromBoxDecoration(BoxDecoration(
+            shape: BoxShape.circle,
+            color: this.color,
+            image: DecorationImage(image: NetworkImage(this.url))));
+  }
+}
+
+class CircleImage extends StatelessWidget{
+  CircleImage(this.data, {this.radial,this.color, this.url=""}):super();
+  final String data;
+  final String url;
+  final double radial;
+  final Color color;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+        child: CircleAvatar(
+          child: CaptionText(
+            this.data,
+            fontsize: 20,
+            color: Colors.amber,
+          ),
+          radius: this.radial,
+          backgroundColor: this.color,
+          backgroundImage: NetworkImage(this.url),
+        ));
   }
 }
